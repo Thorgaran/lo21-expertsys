1cc5378ab64ea656498e4f89a7f7033584557c89
Thorgaran <39826071+Thorgaran@users.noreply.github.com>
2020-12-28 21:44:01 +0100

    Add function to remove facts in a premise
    Change FactBase to be a double linked list
    Add temporary test code to the main function

--- src/main.h
+++ src/main.h
@@ -8,12 +8,14 @@ typedef struct Fact {
 
 typedef struct FactBaseElem {
     Fact fact;
+    struct FactBaseElem *prev;
     struct FactBaseElem *next;
 } FactBaseElem;
 
+/* The FactBase datatype contains a pointer to both the first and the last fact in the list */
 typedef struct FactBase {
-    FactBaseElem *first;
-    FactBaseElem *last;
+    FactBaseElem *head;
+    FactBaseElem *tail;
 } FactBase;
 
 typedef struct Rule {
@@ -29,5 +31,15 @@ typedef struct RuleBaseElem {
 typedef struct RuleBase {
     RuleBaseElem *first;
 } RuleBase;
+
+bool test_fact_equality(Fact fact1, Fact fact2);
+
+FactBase FB_insert_head(FactBase premise, Fact fact);
+
+FactBase FB_insert_tail(FactBase premise, Fact fact);
+
+FactBase FB_remove_head(FactBase premise);
+
+FactBase FB_remove_fact(FactBase premise, Fact fact_to_remove);
 
 #endif // MAIN_H
